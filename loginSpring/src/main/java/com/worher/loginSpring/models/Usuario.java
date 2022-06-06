8dcc81ad9e4ce3c706b0072146fffed592416b62
AnaAlbarracinc <anaisabel.albarracin@um.es>
2022-06-06 21:20:35 +0200

    login y registro

--- loginSpring/src/main/java/com/worher/loginSpring/models/Usuario.java
+++ loginSpring/src/main/java/com/worher/loginSpring/models/Usuario.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table( name = "usuari",
+@Table( name = "usuario",
 	uniqueConstraints = {
 		@UniqueConstraint(columnNames = "id"),
 		@UniqueConstraint(columnNames = "email")
